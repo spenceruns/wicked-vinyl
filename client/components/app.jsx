807e1e2f79e0183065693bed51c4ad1da3d1256a
Spencer Allen <spenceruns@gmail.com>
2020-01-15 01:28:17 +0000

    Added an Add to Cart button that sends the selected product to the backend to be added to the user's cart

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,9 +10,11 @@ export default class App extends React.Component {
       view: {
         name: 'catalog',
         params: {}
-      }
+      },
+      cart: []
     };
     this.setView = this.setView.bind(this);
+    this.addToCart = this.addToCart.bind(this);
   }
 
   setView(name, params) {
@@ -25,7 +27,33 @@ export default class App extends React.Component {
   }
 
   checkForDetailsPage() {
-    return (this.state.view.name === 'details') ? <ProductDetails productId={this.state.view.params.productId} setView={this.setView} /> : <ProductList setView={this.setView} />;
+    return (this.state.view.name === 'details') ? <ProductDetails productId={this.state.view.params.productId} setView={this.setView} addToCart={this.addToCart} /> : <ProductList setView={this.setView} />;
+  }
+
+  getCartItems() {
+    fetch('/api/cart')
+      .then(result => result.json())
+      .then(data => this.setState({ cart: data }));
+  }
+
+  addToCart(product) {
+    fetch(`/api/cart/${product.productId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    })
+      .then(result => result.json())
+      .then(data => {
+        const newList = this.state.cart;
+        newList.push(data);
+        this.setState({ cart: newList });
+      });
+  }
+
+  componentDidMount() {
+    this.getCartItems();
   }
 
   render() {
@@ -33,7 +61,7 @@ export default class App extends React.Component {
       <div className="container-fluid">
         <header className="row">
           <div className="col-12 navbar navbar-dark bg-dark">
-            <Header />
+            <Header numberInCart={this.state.cart.length} />
           </div>
         </header>
         <div className="row bg-light">
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 function Header(props) {
   return (
-    <div className="container d-flex justify-content-between">
-      <div className="navbar-brand text-white"><i className="fa fa-dollar-sign mr-2"></i><strong>Wicked Sales</strong></div>
+    <div className="container d-flex justify-content-between text-white">
+      <div className="navbar-brand"><i className="fa fa-dollar-sign mr-2"></i><strong>Wicked Sales</strong></div>
+      <div>{props.numberInCart}<i className='fa fa-shopping-cart ml-2'></i></div>
     </div>
   );
 }
