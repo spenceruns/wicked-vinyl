42900cbe91fdb42c31823a6b90faa3f397cdd250
spenceruns <spenceruns@gmail.com>
2020-02-10 03:48:44 +0000

    removed strict address regex check and replaced it with one that checks for an address without symbols. Started confirmation form page

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -11,7 +11,7 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       view: {
-        name: 'confirmation',
+        name: 'vinyl',
         params: {}
       },
       showCart: false,
